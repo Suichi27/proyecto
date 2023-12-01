601159bfe7c99d0f66c9c2aec59e35587afcd12d
MarcoAranda <umauricio2705@gmail.com>
2023-12-01 18:22:17 -0500

    Cambiada la pagina principal y lista pacientes

--- src/app/vistas/pacientes/pacientes.component.ts
+++ src/app/vistas/pacientes/pacientes.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ApiService } from '../../servicios/api/api.service';
+import { Router } from '@angular/router';
+import { ListapacientesI } from '../../modelo/listapacientes.interface';
+
 
 @Component({
   selector: 'app-pacientes',
   templateUrl: './pacientes.component.html',
   styleUrl: './pacientes.component.css'
 })
-export class PacientesComponent {
+export class PacientesComponent implements OnInit {
+
+  pacientes:ListapacientesI[ ] = [ ];
+
+  constructor(private api:ApiService, private router:Router){}
+
+  ngOnInit(): void {
+    this.api.getAllPatients(1).subscribe(data=>{
+      this.pacientes = data;
+    })
+  }
+
+  editarPaciente(id:any){
+    this.router.navigate(['editarP',id]);
+  }
+
+  nuevoPaciente(){
+    this.router.navigate(['nuevoP']);
+  }
 
 }
